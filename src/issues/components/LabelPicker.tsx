1884c5bbc7c77056cecfc25bc69c836706fffe97
Ines Alexandra Rodriguez <inrodriguez@lisit.cl>
2024-02-29 12:30:03 -0300

    feat: select labels

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,21 +1,28 @@
+import { FC } from 'react';
 import { LoadingIcon } from '../../shared/components/LoadingIcon';
 import { useLabels } from '../hooks/useLabels';
 
-export const LabelPicker = () => {
+interface Props {
+  selectedLabels: string[];
+  onChange: (labelName: string) => void;
+}
+
+export const LabelPicker: FC<Props> = ({ selectedLabels, onChange }) => {
   const labelsQuery = useLabels();
-/**
- * isLoading vs isFetching. Is loading considers the data in cache while isFetching is being refetched.
- */
+  /**
+   * isLoading vs isFetching. Is loading considers the data in cache while isFetching is being refetched.
+   */
   if (labelsQuery.isLoading) {
-    return (<LoadingIcon />);
+    return <LoadingIcon />;
   }
   return (
     <>
       {labelsQuery.data?.map((label) => (
         <span
           key={label.id}
-          className='badge rounded-pill m-1 label-picker'
-          style={{ color: `#${label.color}`, border: `1px solid #${label.color}`,}}
+          className={`badge rounded-pill m-1 label-picker  ${selectedLabels.includes(label.name) ? 'bg-primary' : ''}`}
+          style={{ color: `#${label.color}`, border: `1px solid #${label.color}` }}
+          onClick={() => onChange(label.name)}
         >
           {label.name}
         </span>
